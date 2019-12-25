99a4bfdb8f10aab4fdf270ad0416f3d3b6bed2e1
hackeru <hackeru@hackeru1.local>
2019-12-25 12:51:38 +0200

    Initial commit

---
+++ app/src/main/java/edu/tomerbu/lec2part2/MainActivity.java
@@ -0,0 +1,52 @@
+package edu.tomerbu.lec2part2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Activity properties:
+    private EditText etQuestion;
+    private TextView tvAnswer;
+    private Button btnAsk;
+
+    //an array of all possible answers:
+    private String[] answers = {"Yes", "No", "Maybe"};
+    //random number generator helper class:
+    private Random rand = new Random();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //connect the variables using their id's
+        etQuestion = findViewById(R.id.edit_your_quesion);
+        tvAnswer = findViewById(R.id.text_oracle_answer);
+        btnAsk = findViewById(R.id.button_ask);
+
+        //hide the answer for now:
+        tvAnswer.setVisibility(View.INVISIBLE);
+        //react to the button click:
+        btnAsk.setOnClickListener((v)->{
+            //make the text: yes, no, maybe (randomly)
+            int r = rand.nextInt(answers.length); // 0 - 2 (length = 3)
+
+            //random answer from the array:
+            String ans = answers[r];
+
+            //show the answer in the textView:
+            tvAnswer.setText(ans);
+
+            //show the textView:
+            tvAnswer.setVisibility(View.VISIBLE);
+        });
+    }
+}
